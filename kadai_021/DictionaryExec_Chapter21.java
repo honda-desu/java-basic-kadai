4aeee0ae9f0abf44c4be78aa74924558494473bd
honda-desu <h9.k.honda@gmail.com>
2025-03-28 10:14:23 +0900

    kadai_021をプッシュ

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,22 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+	
+	public static void main(String[]args) {
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+		
+		dictionary.wordPut();
+		
+		//検索ワード配列
+		String[] wordsSearch = {"apple", "banana", "grape", "orange"};
+		
+        for(String word : wordsSearch) {
+        	dictionary.setSearchWord(word);
+        	dictionary.search();
+        }
+		
+		
+		
+	}
+
+}
